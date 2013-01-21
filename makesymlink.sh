987aa61225cd8ea78c2f49940cbe24387dee8dee
Jyun-Fan Tsai <jyunfan@gmail.com>
2013-01-21 14:37:13 +0800

    Update makesymlink.sh

--- makesymlink.sh
+++ makesymlink.sh
@@ -1,31 +1,7 @@
 #!/bin/bash
 ############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired etc in ~/etc
+# Create symbolic links to vim dot file
 ############################
 
-########## Variables
-
-dir=~/etc                    # etc directory
-olddir=~/etc_old             # old etc backup directory
-files="vimrc vim"    # list of files/folders to symlink in homedir
-
-##########
-
-# create etc_old in homedir
-echo "Creating $olddir for backup of any existing etc in ~"
-mkdir -p $olddir
-echo "...done"
-
-# change to the etc directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
-# move any existing etc in homedir to etc_old directory, then create symlinks from the homedir to any files in the ~/etc directory specified in $files
-for file in $files; do
-    echo "Moving any existing etc from ~ to $olddir"
-    mv ~/.$file ~/etc_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
+ln -s `pwd`/vimrc ~/.vimrc
+ln -s `pwd`/vim ~/.vim
