2c2a7e6b8726cc643a9058458d94561c76edbbe0
Jyun-Fan Tsai <jyunfan@gmail.com>
2013-02-23 19:00:22 +0800

    Init submodule

--- makesymlink.sh
+++ makesymlink.sh
@@ -2,6 +2,8 @@
 ############################
 # Create symbolic links to vim dot file
 ############################
-
+git submodule init
+git submodule update
 ln -s `pwd`/vimrc ~/.vimrc
 ln -s `pwd`/vim ~/.vim
+
